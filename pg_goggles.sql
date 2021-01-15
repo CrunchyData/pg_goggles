805835f856b4acea343060c5d4c213f3f24b3c5d
Greg Smith <greg.smith@crunchydata.com>
2021-01-15 17:42:48 -0500

    Add basic bgwriter rate implementation. The query doesn't work as a view yet.

--- pg_goggles.sql
+++ pg_goggles.sql
@@ -1,6 +1,9 @@
 -- The redundant DROP/CREATE statements here are to make cutting and pasting
 -- sections of this easier when testing.
 
+-- Buffers are 8192 bytes.
+-- Page size is adjustable at compile time, and some commercial distributions (EDB, others) did 16K block PG releases at one point.
+
 -- Goggles enhanced view
 DROP VIEW IF EXISTS pgg_stat_bgwriter CASCADE;
 CREATE OR REPLACE VIEW pgg_stat_bgwriter AS
@@ -26,7 +29,44 @@ CREATE OR REPLACE VIEW pgp_stat_bgwriter AS
 
 -- Rate oriented view
 DROP VIEW IF EXISTS pgr_stat_bgwriter CASCADE;
+
 CREATE OR REPLACE VIEW pgr_stat_bgwriter AS
-    SELECT
-        *
-    FROM pgg_stat_bgwriter;
+	SELECT
+		sample,
+		runtime,
+		-- TODO Convert timed/req to ratio then I can in theory re-derive the numbers without the originals.
+		--Pretty version should only show the nice versions, so all % instead of counts?
+		checkpoints_timed,checkpoints_req,
+		-- TODO Need to catch 0 case
+		round(seconds::numeric / 60 / (checkpoints_timed + checkpoints_req),3) AS minutes_to_checkpoint,
+		round(8192 * buffers_checkpoint / (1024 * 1024 * seconds)::numeric,3)  AS checkpoint_mbps,
+		round(8192 * buffers_clean / (1024 * 1024 * seconds)::numeric,3) AS clean_mbps,
+		round(8192 * buffers_backend / (1024 * 1024 * seconds)::numeric,3) AS backend_mbps,
+		round(8192 * (buffers_checkpoint + buffers_clean + buffers_backend) / (1024 * 1024 * seconds)::numeric,3) AS total_write_mbps,
+		round(8192 * buffers_alloc / (1024 * 1024 * seconds)::numeric,3) AS alloc_mbps,
+		-- TODO Include raw values or not in rate views?
+		--buffers_alloc,
+		--buffers_checkpoint,
+		--buffers_clean,
+		--buffers_backend,
+		round((maxwritten_clean / seconds)::numeric,3) AS max_clean_per_sec,
+		8192 * buffers_backend_fsync AS bytes_backend_fsync,
+		round((1000 * checkpoint_write_time / (buffers_checkpoint + buffers_clean + buffers_backend))::numeric,3) AS avg_chkp_write_ms,
+		round((1000 * checkpoint_sync_time / (buffers_checkpoint + buffers_clean + buffers_backend))::numeric,3) AS avg_chkp_sync_ms
+	FROM
+	(
+	SELECT
+	  current_timestamp AS sample,
+	  current_timestamp - stats_reset AS runtime,
+	  EXTRACT(EPOCH FROM current_timestamp) - extract(EPOCH FROM stats_reset) AS seconds,
+	  b.*
+	FROM
+	  pg_stat_bgwriter b
+	) bgw
+	;
+
+-- Next gen version based on pgg view
+DROP VIEW IF EXISTS pgn_stat_bgwriter CASCADE;
+CREATE OR REPLACE VIEW pgn_stat_bgwriter AS
+	SELECT * FROM pgg_stat_bgwriter;
+
