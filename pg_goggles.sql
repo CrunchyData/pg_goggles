6079a4481ac003c2561ac9edb052f333a19b8ad3
Greg Smith <greg.smith@crunchydata.com>
2021-01-16 05:09:55 -0500

    Working pg_stat_bgwriter wrapping set.

--- pg_goggles.sql
+++ pg_goggles.sql
@@ -25,80 +25,86 @@ CREATE OR REPLACE VIEW pgg_stat_bgwriter AS
 		pg_stat_get_bgwriter_buf_written_checkpoints() AS writes_checkpoint
 		;
 
--- Pretty view
-DROP VIEW IF EXISTS pgp_stat_bgwriter CASCADE;
-CREATE OR REPLACE VIEW pgp_stat_bgwriter AS
-    SELECT
-        *
-    FROM pgg_stat_bgwriter;
-
--- Rate oriented view against current views
+-- Byte rate oriented view
 -- TODO Include raw values or not in rate views?
-DROP VIEW IF EXISTS pgr_stat_bgwriter CASCADE;
-CREATE OR REPLACE VIEW pgr_stat_bgwriter AS
+DROP VIEW IF EXISTS pgb_stat_bgwriter CASCADE;
+CREATE OR REPLACE VIEW pgb_stat_bgwriter AS
     WITH bgw AS (
         SELECT
-        current_timestamp AS sample,
-        current_timestamp - stats_reset AS runtime,
-        EXTRACT(EPOCH FROM current_timestamp) - extract(EPOCH FROM stats_reset) AS seconds,
-        b.*
+            current_timestamp AS sample,
+            current_timestamp - stats_reset AS runtime,
+            (EXTRACT(EPOCH FROM current_timestamp) - extract(EPOCH FROM stats_reset))::numeric AS seconds,
+            b.*
         FROM
-        pg_stat_bgwriter b
+            pg_stat_bgwriter b
     )
     SELECT
         sample,
         runtime,
-        -- TODO Convert timed/req to ratio then I can in theory re-derive the numbers without the originals.
-        --Pretty version should only show the nice versions, so all % instead of counts?
+        CASE WHEN (checkpoints_timed + checkpoints_req) > 0
+            THEN round((100 * checkpoints_timed / (checkpoints_timed + checkpoints_req))::numeric,3)
+            ELSE 0 END AS checkpoint_timed_pct,
+        CASE WHEN (checkpoints_timed + checkpoints_req) > 0
+            THEN round(seconds / 60 / (checkpoints_timed + checkpoints_req),3)
+            ELSE 0 END AS minutes_to_checkpoint,
+        8192 * buffers_alloc      / seconds AS alloc_bps,
+        8192 * buffers_checkpoint / seconds AS checkpoint_bps,
+        8192 * buffers_clean      / seconds AS clean_bps,
+        8192 * buffers_backend    / seconds AS backend_bps,
+        (buffers_checkpoint + buffers_clean + buffers_backend) / seconds AS total_write_bps,
+        maxwritten_clean / seconds AS max_clean_per_sec,
+        8192 * buffers_backend_fsync AS bytes_backend_fsync,
+        checkpoint_write_time / buffers_checkpoint AS avg_chkp_write_s,
+        checkpoint_sync_time  / buffers_checkpoint AS avg_chkp_sync_s
+    FROM bgw
+    ;
+
+-- Rate oriented view.  Recommended units with MB/s.
+-- TODO Rewrite this to be based on byte version?
+DROP VIEW IF EXISTS pgr_stat_bgwriter CASCADE;
+CREATE OR REPLACE VIEW pgr_stat_bgwriter AS
+    WITH bgw AS (
+        SELECT
+            current_timestamp AS sample,
+            current_timestamp - stats_reset AS runtime,
+            (EXTRACT(EPOCH FROM current_timestamp) - extract(EPOCH FROM stats_reset))::numeric AS seconds,
+            b.*
+        FROM
+            pg_stat_bgwriter b
+    )
+    SELECT
+        sample,
+        runtime,
         checkpoints_timed,checkpoints_req,
-        -- TODO Need to catch 0 case
-        round(seconds::numeric / 60 / (checkpoints_timed + checkpoints_req),3) AS minutes_to_checkpoint,
-        round(8192 * buffers_checkpoint / (1024 * 1024 * seconds)::numeric,3)  AS checkpoint_mbps,
-        round(8192 * buffers_clean / (1024 * 1024 * seconds)::numeric,3) AS clean_mbps,
-        round(8192 * buffers_backend / (1024 * 1024 * seconds)::numeric,3) AS backend_mbps,
-        round(8192 * (buffers_checkpoint + buffers_clean + buffers_backend) / (1024 * 1024 * seconds)::numeric,3) AS total_write_mbps,
-        round(8192 * buffers_alloc / (1024 * 1024 * seconds)::numeric,3) AS alloc_mbps,
-        -- TODO Include raw values or not in rate views?
-        --buffers_alloc,
-        --buffers_checkpoint,
-        --buffers_clean,
-        --buffers_backend,
-        round((maxwritten_clean / seconds)::numeric,3) AS max_clean_per_sec,
+        CASE WHEN (checkpoints_timed + checkpoints_req) > 0
+            THEN round((100 * checkpoints_timed / (checkpoints_timed + checkpoints_req))::numeric,3)
+            ELSE 0 END AS checkpoint_timed_pct,
+        CASE WHEN (checkpoints_timed + checkpoints_req) > 0
+            THEN round(seconds / 60 / (checkpoints_timed + checkpoints_req),3)
+            ELSE 0 END AS minutes_to_checkpoint,
+        round(8192 * buffers_alloc      / (1024 * 1024 * seconds),3) AS alloc_mbps,
+        round(8192 * buffers_checkpoint / (1024 * 1024 * seconds),3) AS checkpoint_mbps,
+        round(8192 * buffers_clean      / (1024 * 1024 * seconds),3) AS clean_mbps,
+        round(8192 * buffers_backend    / (1024 * 1024 * seconds),3) AS backend_mbps,
+        round(8192 * (buffers_checkpoint + buffers_clean + buffers_backend) / (1024 * 1024 * seconds),3) AS total_write_mbps,
+        round(maxwritten_clean / seconds,3) AS max_clean_per_sec,
         8192 * buffers_backend_fsync AS bytes_backend_fsync,
         round((1000 * checkpoint_write_time / buffers_checkpoint)::numeric,3) AS avg_chkp_write_ms,
         round((1000 * checkpoint_sync_time  / buffers_checkpoint)::numeric,3) AS avg_chkp_sync_ms
     FROM bgw
     ;
 
--- Next gen version based on pgg view
-DROP VIEW IF EXISTS pgn_stat_bgwriter CASCADE;
-CREATE OR REPLACE VIEW pgn_stat_bgwriter AS
-    WITH bgw AS (
-        SELECT
-        current_timestamp AS sample,
-        current_timestamp - stats_reset AS runtime,
-        EXTRACT(EPOCH FROM current_timestamp) - extract(EPOCH FROM stats_reset) AS seconds,
-        b.*
-        FROM
-        pgg_stat_bgwriter b
-    )
+-- Pretty view.
+-- Boring for bgwriter, but very useful for relation level data
+DROP VIEW IF EXISTS pgp_stat_bgwriter CASCADE;
+CREATE OR REPLACE VIEW pgp_stat_bgwriter AS
     SELECT
-        sample,
-        -- TODO Convert timed/req to ratio then I can in theory re-derive the numbers without the originals.
-        --Pretty version should only show the nice versions, so all % instead of counts?
-        checkpoints_timed,checkpoints_req,
-        -- TODO Need to catch 0 case
-        round(bytes_alloc      / (1024 * 1024 * seconds)::numeric,3) AS alloc_mbps,
-        round(bytes_checkpoint / (1024 * 1024 * seconds)::numeric,3) AS checkpoint_mbps,
-        round(bytes_clean      / (1024 * 1024 * seconds)::numeric,3) AS clean_mbps,
-        round(bytes_backend    / (1024 * 1024 * seconds)::numeric,3) AS backend_mbps,
-        round((bytes_checkpoint + bytes_clean + bytes_backend) / (1024 * 1024 * seconds)::numeric,3) AS total_write_mbps,
-        round(seconds::numeric / 60 / (checkpoints_timed + checkpoints_req),3) AS minutes_to_checkpoint,
-        round((maxwritten_clean / seconds)::numeric,3) AS max_clean_per_sec,
-        bytes_backend_fsync,
-        round((1000 * checkpoint_write_time / writes_checkpoint)::numeric,3) AS avg_chkp_write_ms,
-        round((1000 * checkpoint_sync_time / writes_checkpoint)::numeric,3) AS avg_chkp_sync_ms
-    FROM bgw;
+        *
+    FROM pgr_stat_bgwriter;
 
--- TODO This would be cleaner as sample code if this mbps version is a reduced version of a straight bps look
+-- Testing
+-- SELECT * FROM pgg_stat_bgwriter;
+-- SELECT * FROM pgb_stat_bgwriter;
+-- SELECT * FROM pgr_stat_bgwriter;
+-- SELECT * FROM pgp_stat_bgwriter;
 
